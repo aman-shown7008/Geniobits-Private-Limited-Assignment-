2ac57408a67001922ca70989295b15b52668db01
Aman Mishra <74037648+aman-shown7008@users.noreply.github.com>
2023-06-04 15:23:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import PrivateRoute from "./Components/PrivateRoute";
+import Offers from "./Pages/Offers";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Header from "./Components/Header";
+import { Flip, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./Pages/CreateListing";
+import EditListing from "./Pages/EditListing";
+import Listing from "./Pages/Listing";
+import Category from "./Pages/Category";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="/edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="bottom-center"
+        transition={Flip}
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
+  );
+}
+
+export default App;
